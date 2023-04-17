0720030f574b177c70c57f1c085ecba845eb7dae
Santiago Rodriguez <santiago_rod.ramirez@uao.edu.co>
2023-04-17 09:52:16 -0500

    aa

---
+++ db/init.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE almacen2;
+USE almacen2;
+
+CREATE TABLE usuarios (
+  nombre VARCHAR(50),
+  email VARCHAR(50),
+  usuario VARCHAR(50),
+  password VARCHAR(20),
+  primary key(usuario)
+);
+
+CREATE TABLE productos (
+  id INT auto_increment,
+  nombre VARCHAR(50),
+  precio DECIMAL(10,2),
+  inventario INT,
+  primary key(id)
+);
+
+CREATE TABLE ordenes (
+  id INT auto_increment,
+  nombreCliente VARCHAR(50),
+  emailCliente VARCHAR(50),
+  totalCuenta DECIMAL(10,2),
+  fecha DATETIME DEFAULT(now()),
+  primary key (id)
+);
